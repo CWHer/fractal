d3f0a2917a5c4bf9b330a3875a2be9f6343a146e
cwher <cwher@outlook.com>
2021-05-09 15:16:36 +0800

    fix: fix typo and add gitignore

--- fractals.py
+++ fractals.py
@@ -15,14 +15,14 @@ class SierpinskiFractal():
         fractal_size = 3**depth
         self.fractal = np.zeros([fractal_size, fractal_size], dtype=np.int32)
 
-        def drawFractal(left_upper, depth):
+        def drawFractal(left_up, depth):
             if depth == 0: return
-            left, upper = left_upper
+            left, up = left_up
             block_size = 3**(depth - 1)
             for r in range(3):
                 for c in range(3):
                     x = left + c * block_size
-                    y = upper + r * block_size
+                    y = up + r * block_size
                     if r == 1 and c == 1:
                         for i in range(block_size):
                             for j in range(block_size):
